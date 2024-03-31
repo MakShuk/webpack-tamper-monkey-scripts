70e3978cae329b17686f2dbf967ac0e4b070fe23
Maksim Shuklin <makshuk@bk.ru>
2024-03-31 10:58:21 +0300

    Добавлены зависимости и настройки для Terser Webpack Plugin и Dotenv Webpack Plugin

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// ==UserScript==
+// @name         dzen.ru
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-03
+// @description  try to take over the world!
+// @author       You
+// @match        *://dzen.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dzen.ru
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  'use strict';
+  const style = document.createElement('style');
+  console.log('style', style);
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	mode: 'production', // Может быть 'production' или 'development'
-	entry: './src/main.ts',
-	output: {
-		filename: 'script.js',
-		path: path.resolve(__dirname, 'build'),
-	},
-	resolve: {
-		extensions: ['.ts', '.js'],
-	},
-	plugins: [
-		new Dotenv(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.scss$/, // Добавляем правило для файлов SCSS
-				use: [
-					'style-loader', // Вставляет стили в DOM при запуске приложения (подходит для разработки)
-					'css-loader', // Загружает CSS в JavaScript
-					'sass-loader', // Компилирует SCSS в CSS
-				],
-			},
-		],
-	},
-	watch: true,
+  mode: 'production', // Может быть 'production' или 'development'
+  entry: './src/main.ts',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: true,
+          },
+        },
+        extractComments: true,
+      }),
+    ],
+  },
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new Dotenv()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/, // Добавляем правило для файлов SCSS
+        use: [
+          'style-loader', // Вставляет стили в DOM при запуске приложения (подходит для разработки)
+          'css-loader', // Загружает CSS в JavaScript
+          'sass-loader', // Компилирует SCSS в CSS
+        ],
+      },
+    ],
+  },
+  watch: true,
 };
